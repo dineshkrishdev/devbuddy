395ed46bc843482af283f0a3ba382363d3d14ed6
Dinesh Krishnan <dinesh@dineshkrish.com>
2017-11-04 16:57:16 +0530

    Added angular-ui-router

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var app = angular.module("devBuddy", ["ui.router"]);
+ 
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise("/main")
+        
+    $stateProvider
+
+    .state('main', {
+        url: "/main",
+        templateUrl: "pages/main.html" 
+    })
+    .state("calculator", {
+        url: "/calculator",
+        templateUrl : "pages/calculator.html"
+    })
+    .state("converter", {
+        url: "/converter",
+        templateUrl : "pages/converter.html"
+    })
+    .state("formatter", {
+        url: "/formatter",
+        templateUrl : "pages/formatter.html"
+    })
+    .state("minifier", {
+        url: "/minifier",
+        templateUrl : "pages/minifier.html"
+    })
+    .state("spliter", {
+        url: "/spliter",
+        templateUrl : "pages/spliter.html"
+    })
+    .state("string-utils", {
+        url: "/string-utils",
+        templateUrl : "pages/string-utils.html"
+    })
+    .state("validator", {
+        url: "/validator",
+        templateUrl : "pages/validator.html"
+    })
+    .state("viewer", {
+        url: "/viewer",
+        templateUrl : "pages/viewer.html"
+    })
+            
+}]);
