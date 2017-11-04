b4841cc0755ead996b3b2f53caa386b6f417b4d9
Dinesh Krishnan <dinesh@dineshkrish.com>
2017-11-04 12:05:41 +0530

    Developers Supporting Tools

--- js/script.js
+++ js/script.js
@@ -5,7 +5,28 @@ app.config(function($routeProvider) {
     .when("/", {
         templateUrl : "tt.html"
     })
+    .when("/calculator", {
+        templateUrl : "pages/calculator.html"
+    })
+    .when("/converter", {
+        templateUrl : "pages/converter.html"
+    })
     .when("/formatter", {
-        templateUrl : "tt.html"
+        templateUrl : "pages/formatter.html"
+    })
+    .when("/minifier", {
+        templateUrl : "pages/minifier.html"
+    })
+    .when("/spliter", {
+        templateUrl : "pages/spliter.html"
+    })
+    .when("/string_utils", {
+        templateUrl : "pages/string_utils.html"
+    })
+    .when("/validator", {
+        templateUrl : "pages/validator.html"
+    })
+    .when("/viewer", {
+        templateUrl : "pages/viewer.html"
     });
 });
